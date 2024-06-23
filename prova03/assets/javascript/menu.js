6faddf1652c76450fb33173f1f68233587f48411
Paulo Henrique Santos <phqnrz@gmail.com>
2024-06-23 20:31:17 -0300

    menu html + css + js

---
+++ prova03/assets/javascript/menu.js
@@ -0,0 +1,24 @@
+function cadastrar() {
+  alert("Você será direcionado(a).");
+  location.href = './cadastrarVeiculos.html';
+}
+
+function listar() {
+  alert("Você será direcionado(a).");
+  location.href = './listarVeiculos.html';
+}
+
+function buscar() {
+  alert("Você será direcionado(a).");
+  location.href = './buscarVeiculos.html';
+}
+
+function excluir() {
+  alert("Você será direcionado(a).");
+  location.href = './excluirVeiculos.html';
+}
+
+function sair() {
+  alert("Programa encerrado!");
+  location.href = '/prova03';
+}
