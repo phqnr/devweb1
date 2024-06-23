cd94d4358ac54ceeb482391ddb87646dce092bbc
Paulo Henrique Santos <phqnrz@gmail.com>
2024-06-23 20:30:42 -0300

    ajustes login

--- prova03/assets/javascript/login.js
+++ prova03/assets/javascript/login.js
@@ -13,3 +13,18 @@ function login() {
     document.getElementById('senha').value = '';
   }
 }
+
+
+// efeito máquina de escrever
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() =>
+      elemento.innerHTML += letra, 50 * i);
+  });
+}
+
+const nome = document.querySelector(".descricao");
+typeWriter(nome);
+//-----------------------------------------------------------
